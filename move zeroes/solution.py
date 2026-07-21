ce47373f9ec53aadc30f3d4b37e284680db46832
debjeetxyz <purakayasthadebjeet@gmail.com>
2026-07-22 00:58:42 +0530

    Added solutions

--- move zeroes/solution.py
+++ move zeroes/solution.py
@@ -1,1 +1,11 @@
-#solution to be uploaded...
+n = int(input("Enter the number: "))
+num =[]
+for i in range(n):
+    e = int(input("Enter the element: "))
+    num.append(e)
+l = 0 
+for r in range(len(num)):
+    if num[r] != 0:
+        num[r],num[l]=num[l],num[r]
+        l+=1
+print(num)

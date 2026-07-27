deb3847835feb95ea7c9950c2689992127bbffbb
Debjeet Purakayastha <purakayasthadebjeet@gmail.com>
2026-07-27 18:59:01 +0530

    Time: 3 ms (28.97%), Space: 19.1 MB (96.79%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.py
+++ 3978-unique-middle-element/3978-unique-middle-element.py
@@ -1,3 +1,7 @@
 class Solution:
     def isMiddleElementUnique(self, nums: list[int]) -> bool:
-        return nums.count(nums[len(nums)//2]) == 1
+        c = 0
+        for i in nums:
+            if i == nums[len(nums)//2]:
+                c+=1
+        return c == 1
